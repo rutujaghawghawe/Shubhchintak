298196098be0cbe4f470cd53efdd750996380784
rutujaghawghawe <rutuja.ghawghawe@gmail.com>
2023-02-26 23:09:42 +0530

    Chapter 14 completed

---
+++ Python/Chapter 14/conditionals.py
@@ -0,0 +1,13 @@
+n = 5
+print("Hello") if n > 10 else print("Goodbye") if n > 5 else print("Good day")
+
+number = 5
+if number > 2:
+    print("Number is greater than 2.")
+elif number < 2: 
+    print("Number is smaller than 2.")
+else: 
+    print("Number is 2.")
+
+if 2 + 2 == 4:
+    print("I know math!")
