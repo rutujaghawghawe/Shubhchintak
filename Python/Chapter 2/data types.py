e08a052ee7ba686d7070a7e882874ea6fe266cfc
rutujaghawghawe <rutuja.ghawghawe@gmail.com>
2023-02-26 16:34:47 +0530

    Chapter 2 completed

---
+++ Python/Chapter 2/data types.py
@@ -0,0 +1,29 @@
+a_str = 'Hello World'
+print(a_str) 
+print(a_str[0]) 
+print(a_str[0:5])
+
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+
+a = 10 
+b = 10.2 
+c = 3.14j 
+print(type(a))
+print(type(b))
+print(type(c))
+
+list = [123,'abcd',10.2,'d'] 
+list1 = ['hello','world']
+print(list+list1)
+
+dic={'name':'red','age':10}
+print(dic) 
+print(dic['name']) 
+print(dic.values())
+print(dic.keys())
+
+tuple = (123,'hello')
+tuple1 = ('world')
+print(tuple) 
+print(tuple[0]) 
