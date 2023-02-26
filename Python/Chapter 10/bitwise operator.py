e7eae24e97ccbf86379c6601cdd39607590280aa
rutujaghawghawe <rutuja.ghawghawe@gmail.com>
2023-02-26 20:21:20 +0530

    Chapter 10 completed

---
+++ Python/Chapter 10/bitwise operator.py
@@ -0,0 +1,16 @@
+print(~1)
+
+print(60 & 30)
+print(bin(60 & 30))
+
+print(60 ^ 30)
+print(bin(60 ^ 30))
+
+print(60 | 30)
+print(bin(60 | 30))
+
+print(60 >> 30)
+print(bin(60 >> 30))
+
+print(60 << 30)
+print(bin(60 << 30))
