c62d1968ef095a1ef2e1c641b65eb593fdacfd8f
rutujaghawghawe <rutuja.ghawghawe@gmail.com>
2023-03-01 21:13:37 +0530

    Chapter 29 completed

---
+++ Python/Chapter 29/basic input and output.py
@@ -0,0 +1,32 @@
+print("This string will be displayed in the output")
+print("You can print \n escape characters too.")
+
+with open('shoppinglist.txt', 'w') as fileobj:
+    fileobj.write('tomato\npasta\ngarlic')
+
+with open('shoppinglist.txt', 'r') as fp:
+    for line in fp:
+        print(line)
+
+with open("shoppinglist.txt", "r") as fp:
+    lines = fp.readlines()
+for i in range(len(lines)):
+    print("Line " + str(i) + ": " + line) 
+
+with open('shoppinglist.txt') as in_file:
+    content = in_file.read()
+print(content)           
+
+with open('myfile.txt', 'w') as f:
+    f.write("Line 1\n")
+    f.write("Line 2\n")
+    f.write("Line 3\n")
+    f.write("Line 4\n")
+
+with open('my_file.txt', 'w', encoding='utf-8') as f:
+    f.write('utf-8 text')   
+
+with open('fred.txt', 'w') as outfile:
+    s = "I'm Not Dead Yet!"
+    print(s) 
+    print(s, file = outfile)     
