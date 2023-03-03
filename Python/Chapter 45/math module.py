9d63a1a314b8009fb989a96fd410ad9711e033cb
rutujaghawghawe <rutuja.ghawghawe@gmail.com>
2023-03-03 23:44:04 +0530

    Chapter 45 completed

---
+++ Python/Chapter 45/math module.py
@@ -0,0 +1,31 @@
+import math
+
+x = 1.55
+y = -1.55
+
+print(math.floor(x)) 
+print(math.floor(y))
+
+print(math.ceil(x))
+print(math.ceil(y))
+
+print(math.trunc(x))
+print(math.trunc(y))
+
+print(math.hypot(2, 4))
+print(math.radians(45))
+print(math.degrees(math.asin(1)))
+print(math.sin(math.pi / 2))
+print(math.cos(math.pi / 2))
+print(math.atan(float('inf')))
+print(math.atan2(1, 2))
+print(math.sinh(math.pi))
+
+print(math.pow(5, 4))
+print(math.pow(25, 0.5))
+
+print(math.log(math.e))
+print(math.log(1))
+print(math.log(100, 10))
+print(math.log(27, 3))
+
