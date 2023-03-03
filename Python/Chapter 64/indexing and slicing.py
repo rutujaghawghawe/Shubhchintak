7dafd7b822da213b827b197346f294ef052ac29b
rutujaghawghawe <rutuja.ghawghawe@gmail.com>
2023-03-04 01:11:51 +0530

    Chapter 64 completed

---
+++ Python/Chapter 64/indexing and slicing.py
@@ -0,0 +1,27 @@
+a = "abcdefghijklmnopqrstuvwxyz"
+print(a)
+print(a[-1])
+print(a[:])
+print(a[::])
+print(a[3:])
+print(a[:4])
+print(a[2:4])
+print(a[::2])
+print(a[1:4:2])
+print(a[:-1])
+print(a[-1:])
+print(a[24:1:-1])
+print(a[::-1])
+
+lst = [1, 2, 3]
+lst[1:3] = [4, 5]
+print(lst)
+
+lst = [1, 2, 3]
+lst[-2:] = [4, 5, 6]
+print(lst)
+
+arr = ['a', 'b', 'c', 'd']
+print(arr[0])
+print(arr[-2])
+
